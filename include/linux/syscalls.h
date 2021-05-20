518e70c37c139312a05aa2921a28e008e6d5e02e
masonm <maseymoose@gmail.com>
2021-05-20 17:52:38 -0400

    Update syscalls.h

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1377,4 +1377,6 @@ int __sys_getsockopt(int fd, int level, int optname, char __user *optval,
 		int __user *optlen);
 int __sys_setsockopt(int fd, int level, int optname, char __user *optval,
 		int optlen);
+
+asmlinkage long set_seed(void);
 #endif
